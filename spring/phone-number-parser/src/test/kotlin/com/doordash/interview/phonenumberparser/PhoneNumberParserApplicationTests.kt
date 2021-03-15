5e098032eba5fc42f5ae196c28626a34efa00200
sleyzerzon <senya22@yahoo.com>
2021-03-14 20:37:29 -0400

    writing tests

--- spring/phone-number-parser/src/test/kotlin/com/doordash/interview/phonenumberparser/PhoneNumberParserApplicationTests.kt
+++ spring/phone-number-parser/src/test/kotlin/com/doordash/interview/phonenumberparser/PhoneNumberParserApplicationTests.kt
@@ -1,13 +1,32 @@
 package com.doordash.interview.phonenumberparser
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
-class PhoneNumberParserApplicationTests {
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PhoneNumberParserApplicationTests() {
+
+	@Autowired
+	lateinit var restTemplate: TestRestTemplate
 
 	@Test
 	fun contextLoads() {
 	}
+
+	@Test
+	fun `Querying a known id returns expected result`() {
+		val entity = restTemplate.getForEntity<String>("/phone-numbers/386")
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(entity.body).contains("415-415-4155")
+	}
+
 
 }
